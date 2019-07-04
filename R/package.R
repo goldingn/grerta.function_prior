3100d25286d20d908a9774d107a7063ea35149e1
Nick Golding <goldingn35@gmail.com>
2019-07-04 10:28:46 +1000

    flesh out package docs using to do list

--- R/package.R
+++ R/package.R
@@ -1,18 +1,27 @@
 # this R file is just here to produce a package-level helpfile for people to
 # look at when they get started
 
-#' @title What the Package Does (One Line, Title Case)
-#' @name greta.template
-#' 
-#' @description describe your package here, you can re-use the text from DESCRIPTION
-#' 
+#' @title Specifying Priors Over Parametric Functions in greta
+#' @name greta.function_prior
+#'
+#' @description It's common in Bayesian statistical analyses to have prior
+#'   information on summary statistics of a model rather than on the model
+#'   parameters themselves. For example, you may have prior information about
+#'   the probability predicted by a logistic regression under some set of
+#'   covariates, or the timing of peaks in a dynamical model. In these
+#'   situations it can be difficult to determine the priors on the model
+#'   parameters that correspond to these priors on summary statistics.
+#'   greta.function_prior lets you define priors on these summary statistics by
+#'   considering them as priors over parametric functions, and provides a simple
+#'   interface for fitting models.
+#'
 #' @docType package
-#' 
+#'
 #' @importFrom tensorflow tf
 #' @importFrom greta .internals
-#' 
+#'
 #' @examples
-#' 
+#'
 #' # add a simple example here to introduce the package!
 #' 
 NULL
--- tests/testthat.R
+++ tests/testthat.R
@@ -1,3 +1,3 @@
 library(testthat)
-library(greta.template)
-test_check("greta.template")
+library(greta.function_prior)
+test_check("greta.function_prior")
